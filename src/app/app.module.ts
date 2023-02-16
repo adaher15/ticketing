8545eb39c2e6b011f6ade11934372a2aebec3f9d
kdaher_mntv <kdaher@momentivve.ai>
2023-02-16 11:22:49 -0500

    Submit latest code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
